ed642bdf32a41e4073cb6baaaaba32c9f8546d58
sanjeev2201 <sanjeevkumarrai132@gmail.com>
2022-10-27 22:03:36 +0530

    main branch

---
+++ python_file_1/program_1.py
@@ -0,0 +1,17 @@
+print("I am in the main branch")
+class Cexdriver:
+    def __init__(self):
+        self.driver=self.get_driver()
+    def get_driver(self):
+        driver={"sanjeev":"active"}
+        return driver
+    def disp_driver(self):
+        print(f"{self.driver}")
+n=0
+while True:
+    cex=Cexdriver()
+    cex.disp_driver()
+    n+=1
+    if n==2:
+        break
+
